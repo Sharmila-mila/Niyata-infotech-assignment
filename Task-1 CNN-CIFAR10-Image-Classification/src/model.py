1938ab9266df7915ca49ac042d69a10b33942bfa
sharmila.V <sharmilavinayagam2023@gmail.com>
2026-05-18 19:23:37 +0530

    Update model.py

--- Task-1 CNN-CIFAR10-Image-Classification/src/model.py
+++ Task-1 CNN-CIFAR10-Image-Classification/src/model.py
@@ -4,90 +4,37 @@ from tensorflow.keras import models, layers
 def build_cnn_model():
 
     model = models.Sequential()
-    model.add(
-        layers.Conv2D(
-            32,
-            (3, 3),
-            activation='relu',
-            padding='same',
-            input_shape=(32, 32, 3)
-        )
-    )
+    model.add(layers.Conv2D(32,(3, 3),activation='relu',padding='same',input_shape=(32, 32, 3)))
 
     model.add(layers.BatchNormalization())
 
-    model.add(
-        layers.Conv2D(
-            32,
-            (3, 3),
-            activation='relu',
-            padding='same'
-        )
-    )
+    model.add(layers.Conv2D(32,(3, 3),activation='relu',padding='same'))
 
     model.add(layers.MaxPooling2D((2, 2)))
 
     model.add(layers.Dropout(0.25))
-    model.add(
-        layers.Conv2D(
-            64,
-            (3, 3),
-            activation='relu',
-            padding='same'
-        )
-    )
+    model.add(layers.Conv2D(64,(3, 3),activation='relu',padding='same'))
 
     model.add(layers.BatchNormalization())
 
-    model.add(
-        layers.Conv2D(
-            64,
-            (3, 3),
-            activation='relu',
-            padding='same'
-        )
-    )
-
+    model.add(layers.Conv2D(64,(3, 3),activation='relu',padding='same'))
+    
     model.add(layers.MaxPooling2D((2, 2)))
 
     model.add(layers.Dropout(0.25))
-    model.add(
-        layers.Conv2D(
-            128,
-            (3, 3),
-            activation='relu',
-            padding='same'
-        )
-    )
+    model.add(layers.Conv2D(128,(3, 3),activation='relu',padding='same'))
 
     model.add(layers.BatchNormalization())
 
-    model.add(
-        layers.Conv2D(
-            128,
-            (3, 3),
-            activation='relu',
-            padding='same'
-        )
-    )
+    model.add(layers.Conv2D(128,(3, 3),activation='relu',padding='same'))
 
     model.add(layers.MaxPooling2D((2, 2)))
 
     model.add(layers.Dropout(0.3))
     model.add(layers.Flatten())
-    model.add(
-        layers.Dense(
-            256,
-            activation='relu'
-        )
-    )
+    model.add(layers.Dense(256,activation='relu'))
 
     model.add(layers.Dropout(0.5))
-    model.add(
-        layers.Dense(
-            10,
-            activation='softmax'
-        )
-    )
+    model.add(layers.Dense(10,activation='softmax'))
 
     return model
