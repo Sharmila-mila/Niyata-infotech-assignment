776fb3dc58e0eee947416213d7fcf7ff898e0369
sharmila.V <sharmilavinayagam2023@gmail.com>
2026-05-18 19:27:28 +0530

    Update main.py

--- Task-1 CNN-CIFAR10-Image-Classification/main.py
+++ Task-1 CNN-CIFAR10-Image-Classification/main.py
@@ -9,18 +9,13 @@ from src.train import train_model
 from src.evaluate import evaluate_model
 from src.predict import predict_multiple_images
 
-
 def main():
 
     model, history, X_test, y_test = train_model()
 
     evaluate_model(model, X_test, y_test)
 
-    predict_multiple_images(
-        model,
-        "test_images"
-    )
-
+    predict_multiple_images(model,"test_images")
 
 if __name__ == "__main__":
     main()
